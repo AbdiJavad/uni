40152be2e64cbfd310c85ea881d36123f0df4705
AbdiJavad <Mirjavadabdi@gmail.com>
2023-11-16 23:25:58 +0330

    Updated

--- src/main/java/com/javad/uni/model/User.java
+++ src/main/java/com/javad/uni/model/User.java
@@ -2,7 +2,17 @@ package com.javad.uni.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "users")
+    private long Id;
+    private String username;
+    private String password;
 
 }
+
