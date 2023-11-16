7597dd21f0f585e1915166e3e0e3365980255f08
AbdiJavad <Mirjavadabdi@gmail.com>
2023-11-16 23:49:00 +0330

    Updated

--- src/main/java/com/javad/uni/model/User.java
+++ src/main/java/com/javad/uni/model/User.java
@@ -1,18 +1,27 @@
 package com.javad.uni.model;
 
+import jdk.nashorn.internal.runtime.Debug;
+import lombok.Builder;
 import lombok.Data;
-
 import javax.persistence.*;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Entity;
 
 @Data
 @Entity
+@Builder
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "users")
+
+
     private long Id;
     private String username;
     private String password;
 
+    public User() {
+    }
 }
 
