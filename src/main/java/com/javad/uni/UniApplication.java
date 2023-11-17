6c718c0e0f648c3f61809424bc1f15785235ccbc
AbdiJavad <Mirjavadabdi@gmail.com>
2023-11-17 16:23:38 +0330

    Updated

---
+++ src/main/java/com/javad/uni/UniApplication.java
@@ -0,0 +1,23 @@
+package com.javad.uni;
+
+import com.javad.uni.model.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniApplication.class, args);
+        User user = new User();
+        User user1 = User
+                .builder()
+                .id(1L)
+                .username("Ali")
+                .password("1234")
+                .build();
+
+
+    }
+
+}
