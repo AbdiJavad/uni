8fdc364957107fcef4352ca96794e17565b3ab95
JavadAbdi <Mirjavadabdi@gmail.com>
2024-07-04 23:36:52 +0330

    Updated,Changed

--- src/main/java/com/javad/uni/controller/UserController.java
+++ src/main/java/com/javad/uni/controller/UserController.java
@@ -16,7 +16,7 @@ import java.util.Optional;
 @RequestMapping("user")
 
 public class UserController {
-    
+
     @Autowired
     @Qualifier("userServiceImpl")
     private UserService userService;
